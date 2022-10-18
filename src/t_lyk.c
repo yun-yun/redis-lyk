91121a5c34d90a6aca69d70c3ae5bad8053129b9
luyunkui <luyunkui@58.com>
2022-10-18 15:39:01 +0800

    commit

--- src/t_lyk.c
+++ src/t_lyk.c
@@ -7,10 +7,14 @@
 
 void lykCommand(client *c) {
 //    lykGenericCommand(c);
+    char *name = c->argv[1]->ptr;
+    size_t len = sdslen(name);
+    serverLog(LL_WARNING, "lyk :" + len);
+
     char *hello;
     char buf[] = "hello, i'm lyk";
     hello = buf;
-    robj *o = createStringObject(hello, 14);
+    robj *o = createStringObject(hello, sizeof(buf) - 1);
 
 
     addReplyBulk(c, o);
